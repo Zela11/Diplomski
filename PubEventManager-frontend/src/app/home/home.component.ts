b6e6dd6e77e26d64cdb0ec9dc643b73e5ed0cace
Zela11 <lukazelovic@gmail.com>
2024-09-07 04:49:27 +0200

    Added image slider for home page

--- PubEventManager-frontend/src/app/home/home.component.ts
+++ PubEventManager-frontend/src/app/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { TokenStorageService } from '../services/user/token.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  {
+export class HomeComponent implements OnInit {
+  counter = 1; // Brojač za slajdove
 
+  ngOnInit(): void {
+    this.autoSlide(); // Pozovi funkciju za automatsku promenu slajdova
+  }
+
+  autoSlide(): void {
+    setInterval(() => {
+      const radioBtn = document.getElementById('radio' + this.counter) as HTMLInputElement;
+      if (radioBtn) {
+        radioBtn.checked = true; // Selektuj sledeći slajd
+      }
+      this.counter++;
+      if (this.counter > 4) {
+        this.counter = 1; // Vrati se na prvi slajd nakon poslednjeg
+      }
+    }, 5000); // Menjanje slajda svakih 5 sekundi
+  }
 }
